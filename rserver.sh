f2b8f59d7d19ba9a8af2d287dc9fa8a7f39eaf3b
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-03 22:52:34 -0400

    Update rserver.sh

--- rserver.sh
+++ rserver.sh
@@ -3,4 +3,4 @@
 ### In rserver.sh (make sure this file is chmod +x):
 # `/sbin/setuser xxxx` runs the given command as the user `xxxx`.
 # If you omit that part, the command will be run as root.
-exec /usr/lib/rstudio-server/bin/rserver >>/var/log/mysqld.log 2>&1
+exec /usr/lib/rstudio-server/bin/rserver >>/var/log/rserver.log 2>&1
