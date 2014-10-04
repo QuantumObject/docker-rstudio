ca3cc4dd9aab3ab06593eacdedd666a569c7a3c8
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-03 22:59:35 -0400

    Update rserver.sh

--- rserver.sh
+++ rserver.sh
@@ -3,4 +3,5 @@
 ### In rserver.sh (make sure this file is chmod +x):
 # `/sbin/setuser xxxx` runs the given command as the user `xxxx`.
 # If you omit that part, the command will be run as root.
+
 exec /usr/lib/rstudio-server/bin/rserver >>/var/log/rserver.log 2>&1
