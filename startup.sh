2c7af251e7b309d55a081675e658e9837426bd4c
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-06 12:38:39 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,12 +4,10 @@ set -e
 
 if [ -f /etc/configured ]; then
   echo 'already configured'
-  #/usr/lib/rstudio-server/bin/rserver >>/var/log/rserver.log 2>&1
 else
   #code that need to run only one time ....
   #needed for fix problem with ubuntu and cron
   ln -s /etc/apparmor.d/rstudio-server /etc/apparmor.d/disable/
   update-locale
-  #/usr/lib/rstudio-server/bin/rserver >>/var/log/rserver.log 2>&1
   date > /etc/configured
 fi
