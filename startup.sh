73e0077178ecb7993acbf9164bf7a47a0f41cc20
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-10-23 01:07:25 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ if [ -f /etc/configured ]; then
 else
   #code that need to run only one time ....
   #needed for fix problem with ubuntu and cron
-  ln -s /etc/apparmor.d/rstudio-server /etc/apparmor.d/disable/
+  #ln -s /etc/apparmor.d/rstudio-server /etc/apparmor.d/disable/
   update-locale
   date > /etc/configured
 fi
